9900f805ad47b297e1f3bef4c3de3ecf7ad193d2
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-09-03 21:45:36 -0300

    Vinculando Filmes com Mongo em Nuvem

--- exemplo-filmes/models/conn/conn.js
+++ exemplo-filmes/models/conn/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function Conn(url, port, banco){
-    mongoose.connect(`mongodb://${url}:${port}/${banco}`,{
+    mongoose.connect(`mongodb+srv://dbUser:blueUser123@blue-db.3qloa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
